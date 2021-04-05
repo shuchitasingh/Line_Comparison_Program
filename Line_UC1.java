c125bf3d71054309f72b4df02076daa893a9c17c
shuchitasingh <singhshuchita5@gmail.com>
2021-04-05 15:42:57 +0530

    Added:calculate length

---
+++ Line_UC1.java
@@ -0,0 +1,20 @@
+public class LineComparison {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Line Comparison Computation Program");
+
+		int X1,X2,Y1,Y2;
+		double lengthOfTheLine;
+		X1 = 1;
+		X2 = 2;
+		Y1 = 3;
+		Y2 = -6;
+		lengthOfTheLine = Math.sqrt((X2 - X1) * (X2 - X1) + (Y2 - Y1) * (Y2 - Y1));
+		System.out.println("length of the line (" + X1 + "," + Y1 + ")(" + X2 + "," + Y2 + ") is: " + lengthOfTheLine);
+	}
+
+
+
+}
+© 2021 GitHub, Inc.
